3bea0742f0c7f7001e50eda65aa2877086500fbd
Alessio Santoro <alexi.santoro@gmail.com>
2026-02-19 11:05:28 +0100

    Added FontAwesome icon instead of text

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faShuffle} from '@fortawesome/free-solid-svg-icons';
 import {useState} from 'react';
 import recipes from './collections/recipes.json';
 import CategorySection from './utils/CategorySection.jsx';
@@ -6,27 +8,32 @@ import {getShuffledData} from "./utils/SortUtils.jsx";
 
 function App() {
 
-  const [shuffledRecipes, setShuffledRecipes] = useState(getShuffledData(recipes));
+  const [shuffledRecipes, setShuffledRecipes] = useState(
+      getShuffledData(recipes));
 
   const handleShuffleClick = () => {
     setShuffledRecipes(getShuffledData(recipes));
   };
 
   return (
-      <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
-        <h1 style={{ display: 'flex', alignItems: 'center', gap: '10px', margin: 0 }}>
-          <img src={logo} alt="Logo" style={{ width: '60px' }} />
+      <div style={{padding: '20px', fontFamily: 'sans-serif'}}>
+        <h1 style={{
+          display: 'flex',
+          alignItems: 'center',
+          gap: '10px',
+          margin: 0
+        }}>
+          <img src={logo} alt="Logo" style={{width: '60px'}}/>
           Orbitmeal
         </h1>
-        <br/>
         <button
             onClick={handleShuffleClick}
             className="shuffle-button"
         >
-          🔀 Shuffle Recipes
+          <FontAwesomeIcon icon={faShuffle} style={{marginRight: '8px'}}/>
+          <span className="button-text"> Shuffle Recipes</span>
         </button>
         <br/>
-
         {Object.entries(shuffledRecipes).map(([categoryName, dishes]) => (
             <CategorySection
                 key={categoryName}
